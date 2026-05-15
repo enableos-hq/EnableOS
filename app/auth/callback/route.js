f793862d9fb0225bf30f811535be91648301dc1c
enableos-hq <enableos.hq@gmail.com>
2026-05-15 12:39:23 +0530

    Update route.js

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -9,7 +9,8 @@ export async function GET(request) {
   if (code) {
     const supabase = createClient(
       'https://zurkzjhctyfhqcztimnf.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1cmt6amhjdHlmaHFjenRpbW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzYzMTQsImV4cCI6MjA5MjI1MjMxNH0.a_x5O5JZ0YPHs9KMLPCTXnDxuQdd5uhhdaHH8a1jaQk'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1cmt6amhjdHlmaHFjenRpbW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzYzMTQsImV4cCI6MjA5MjI1MjMxNH0.a_x5O5JZ0YPHs9KMLPCTXnDxuQdd5uhhdaHH8a1jaQk',
+      { auth: { persistSession: false } }
     )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) return NextResponse.redirect(`${origin}${next}`)
