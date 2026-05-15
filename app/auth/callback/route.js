95094de3ab5560bc27694eeefb908493c690f9cd
enableos-hq <enableos.hq@gmail.com>
2026-05-15 12:35:23 +0530

    Update route.js

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,4 +1,4 @@
-import { createClient } from '../../../lib/supabase'
+import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
 export async function GET(request) {
@@ -7,7 +7,10 @@ export async function GET(request) {
   const next = searchParams.get('next') ?? '/app'
 
   if (code) {
-    const supabase = createClient()
+    const supabase = createClient(
+      'https://zurkzjhctyfhqcztimnf.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1cmt6amhjdHlmaHFjenRpbW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzYzMTQsImV4cCI6MjA5MjI1MjMxNH0.a_x5O5JZ0YPHs9KMLPCTXnDxuQdd5uhhdaHH8a1jaQk'
+    )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) return NextResponse.redirect(`${origin}${next}`)
   }
